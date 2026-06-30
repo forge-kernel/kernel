edb02b8b2117e5a0f334ae6fe63931a97375ba1f
Jeremias Nunez <jeremias2@gmail.com>
2026-06-30 00:16:58 -0400

    added support for repeatble in provides attribute

--- Core/Module/Attributes/Provides.php
+++ Core/Module/Attributes/Provides.php
@@ -6,7 +6,7 @@ namespace Forge\Core\Module\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class Provides
 {
     public function __construct(
